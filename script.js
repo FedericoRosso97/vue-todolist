95f9b98a3131bccdd4c85d6fe787c072aea48de5
FedericoRosso97 <federicorosso97@gmail.com>
2023-06-06 15:25:05 +0200

    stampo in pagina lista di cose da fare

--- script.js
+++ script.js
@@ -3,7 +3,32 @@ const {createApp}= Vue;
 createApp({
     data(){
         return{
-            
+            toDo:[
+                {
+                    text:'comprare caramelle',
+                    done:true,
+                },
+                {
+                    text:'pattinare',
+                    done:false,
+                },
+                {
+                    text:'decidere cosa fare a capodanno',
+                    done:false,
+                },
+                {
+                    text:'marinare le aringhe',
+                    done:true,
+                },
+                {
+                    text:'',
+                    done:true,
+                },
+                {
+                    text:'',
+                    done:true,
+                },
+            ],
             methods:{
 
             }
